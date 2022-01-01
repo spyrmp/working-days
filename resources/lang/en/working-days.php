401faf6564191007905524e479e6a59fa2abb850
spyrm <spyros@onecode.gr>
2022-01-01 03:09:35 +0200

    first commit

---
+++ resources/lang/en/working-days.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    "non_working_day" => "The field must be non working day.",
+    "is_not_working_day" => "The field must be working day.",
+];
---
+++ src/Rules/AbstractValidation.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Spyrmp\WorkingDays\Rules;
+
+
+
+
+abstract class AbstractValidation
+{
+
+    protected $attribute;
+
+
+    protected $format;
+
+    public function __construct()
+    {
+        $this->format = config("working-day.format");
+    }
+
+
+}
